0a1d6488895e826684430a959f0e00df01434a9e
theprajin <prazenthekiller@gmail.com>
2021-02-07 14:16:05 +0545

    admin user created

--- carcentral/settings.py
+++ carcentral/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'carcentral.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'superuser',
+        'HOST': 'localhost',
     }
 }
 
