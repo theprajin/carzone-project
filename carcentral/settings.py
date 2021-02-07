799dba7386fd5c81b11762e14d541c6003bff900
theprajin <prazenthekiller@gmail.com>
2021-02-07 16:53:01 +0545

    completed teams model and modified admin site for it

--- carcentral/settings.py
+++ carcentral/settings.py
@@ -129,3 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carcentral/static'),
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from .models import Team
+
+teams = Team.objects.all()
+data = {
+    'teams': teams,
+}
 
 def home(request):
-    return render(request, 'pages/home.html')
+    return render(request, 'pages/home.html', data)
 
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    return render(request, 'pages/about.html', data)
 
 def services(request):
     return render(request, 'pages/services.html')
